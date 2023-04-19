bdb86f805b863b3b26ec0dc03417c393a88d8b17
ToritseTuedor <torisevalerie@gmail.com>
2023-04-18 20:25:26 -0400

    updated JSON file,halfway through displaying data through firebase

--- script.js
+++ script.js
@@ -1,41 +1,84 @@
+// 
 import app from "./firebaseConfig.js";
 import {
   getDatabase,
   ref,
   set,
+  push,
+  onValue
 } from "https://www.gstatic.com/firebasejs/9.19.1/firebase-database.js";
 
+// the reference to the database app 
 const database = getDatabase(app);
-const dbRef = ref(database);
+// reference to the products in our database 
+const productRef = ref(database, '/inventory');
 
-const addToDatabase = (key, value) => {
-  const customRef = ref(database, key);
-  set(customRef, value);
-};
+// displaying our product data on the page emitted from firebase on the page 
+onValue(productRef, (data)=> {
+  // use onValue method to listen for changes, grabbing the data and getting a snapshot of the data 
+  const productData =data.val();
 
-// Display the number of items currently added to the user's cart. 
+  for (let key in productData) {
+    console.log(productData[key]);
+    //creating the html to append on the page
+    /* <div id = plant1>
+      <img src= url alt ='' />
+      <button class = "add-to-cart-""> <img src= url alt ='' />
+      </button>
+      <p></p>
+      <p class ="prices"></p>
+    </div>
+    */
+   // create variables to store some elements 
+   const productImgUrl = productData[key].image;
+   const productImgAlt = productData[key].alt;
+   const buttonImgUrl = productData[key].icon;
+   const buttonImgAlt = productData[key].iconAlt;
+   
+  }
+}) 
 
-// Select the add to cart buttons on the page, specifically the image section 
-const addToCartButton = document.querySelectorAll(`.add-to-cart- img`);
 
-//create a ref to the cartCount in the database 
 
-const cartCountRef = ref (database,'cartCount')
 
-cartCountRef.on("value", (snapshot) => {
-  const cartCount = snapshot.val();
-  //Update the cart count display here
-});
 
-//add an event listener to the add to cart buttons so that when the browser hears a click 
 
-addToCartButton.forEach(function(button){ 
-  button.addEventListener(`click`, function (e) {
+
+
+
+
+
+// const addToDatabase = (key, value) => {
+//   const customRef = ref(database, key);
+//   set(customRef, value);
+// };
+
+// // Display the number of items currently added to the user's cart. 
+
+// // Select the add to cart buttons on the page, specifically the image section 
+// const addToCartButton = document.querySelectorAll(`.add-to-cart- img`);
+
+// //create a ref to the cartCount in the database 
+
+// const cartCountRef = ref (database,'cartCount')
+
+// cartCountRef.on("value", (snapshot) => {
+//   const cartCount = snapshot.val();
+//   //Update the cart count display here
+
+
+  
+// });
+
+// //add an event listener to the add to cart buttons so that when the browser hears a click 
+
+// addToCartButton.forEach(function(button){ 
+//   button.addEventListener(`click`, function (e) {
     
 
 
-  });
-});
+//   });
+// });
 
 
 
