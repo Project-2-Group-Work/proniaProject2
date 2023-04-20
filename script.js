fa6201e9c4d3a1ce0d4b9ffc4a32d931442b5662
ToritseTuedor <torisevalerie@gmail.com>
2023-04-20 17:07:55 -0400

    mvp complete

--- script.js
+++ script.js
@@ -74,3 +74,43 @@ onValue(productRef, (data) => {
     document.querySelector("#product-items-container").append(divItem);
   }
 });
+
+
+// code for adding items to the cart
+// event listener for the div container containing the products which contains the buttons (* global variable used here)
+productDivContainer.addEventListener("click", (event) => {
+  if (event.target.className === "shopping-cart-button-img");
+  {
+
+    addToCart(event.target.parentElement.parentElement.id)
+    
+  }
+  
+});
+
+const addToCart = ((selectedItem)=>{ 
+  console.log(selectedItem);
+  const selectedProductItem = ref(database, `/inventory/${selectedItem}`);
+
+  //now we need to get the data stored at that specific location 
+    get(selectedProductItem).then((snapshot) => {
+    const productData = snapshot.val();
+    console.log(snapshot.val());
+    const CartItem = {
+      alt: productData.alt,
+      imgUrl: productData.image,
+      id: productData.id,
+    };
+    push(cartCountRef, CartItem);
+    
+  })
+})
+
+//display the number of item in the cart in the notifications section 
+onValue(cartCountRef, (data)=> { 
+  const itemCount = data.val();
+  const cartCountNotificationElement = document.querySelector('.cart-counter')
+  console.log(itemCount);
+  cartCountNotificationElement.textContent = Object.keys(itemCount).length;
+
+})
